8dd3333a60aa871084c7e2961a57af5cb6dee448
Romstar744 <romstar742@gmail.com>
2024-04-10 22:50:14 +0300

    3

---
+++ app/src/main/java/com/starostin/shop2/Category.kt
@@ -0,0 +1,3 @@
+package com.starostin.shop2
+
+data class Category(val id: Int, val name: String)
--- app/src/main/java/com/starostin/shop2/ItemActivity.kt
+++ app/src/main/java/com/starostin/shop2/ItemActivity.kt
@@ -1,7 +1,9 @@
 package com.starostin.shop2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class ItemActivity : AppCompatActivity() {
@@ -11,6 +13,12 @@ class ItemActivity : AppCompatActivity() {
 
         val title: TextView = findViewById(R.id.item_list_title_one)
         val text: TextView = findViewById(R.id.item_list_text)
+        val linkToCart: Button = findViewById(R.id.button_buy)
+
+        linkToCart.setOnClickListener{
+            val intent = Intent (this, CartActivity::class.java)
+            startActivity(intent)
+        }
 
         title.text = intent.getStringExtra("itemTitle")
         text.text = intent.getStringExtra("itemText")
